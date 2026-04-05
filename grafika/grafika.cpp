f94e2f485bd0f3bb2271932abf51e414b5890d37
mandorpepe <myrlikbeac@gmail.com>
2026-04-05 15:54:19 +0800

    lab 2

--- grafika/grafika.cpp
+++ grafika/grafika.cpp
@@ -3,10 +3,33 @@
 #include <iostream>
 #include "GL\glew.h"
 #include "GLFW/glfw3.h"
-void StartWindow()
-{
 
-}
+float points[] = {
+	-0.5f,  0.5f, 0.0f,  
+	 0.5f,  0.5f, 0.0f,  
+	 0.5f, -0.5f, 0.0f,  
+	-0.5f, -0.5f, 0.0f   
+};
+
+GLuint indices[] = {
+	0, 1, 2,
+	2, 3, 0
+};
+const char* vertexShader =
+"#version 410 core\n"
+"layout (location = 0) in vec3 vp;"
+"void main() {"
+"    gl_Position = vec4(vp, 1.0);"
+"}";
+
+const char* fragmentShader =
+"#version 410 core\n"
+"out vec4 FragColor;"
+"uniform vec4 ourColor;"
+"void main() {"
+"    FragColor = ourColor;"
+"}";
+
 int main()
 {
 	if (!glfwInit()) {
@@ -34,20 +57,51 @@ int main()
 		return 1;
 	}
 
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(points),points,GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShader, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShader, NULL);
+	glCompileShader(fs);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.9f, 0.1f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_QUADS);
+		glUseProgram(shaderProgram);
 
-		glColor3f(0.1f, 0.2f, 0.8f);
+		float timeValue = glfwGetTime();
+		float green = (sin(timeValue) / 2.0f) + 0.5f;
 
-		glVertex2f(-0.5f, 0.5f); // левый верх
-		glVertex2f(0.5f, 0.5f); // правый верх
-		glVertex2f(0.5f, -0.5f); // правый низ
-		glVertex2f(-0.5f, -0.5f); // левый низ
+		int colorLoc = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(colorLoc, 0.1f, green, 0.8f, 1.0f);
 
-		glEnd();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
